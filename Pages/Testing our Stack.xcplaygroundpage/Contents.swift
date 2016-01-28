b3a74102ddf9b6941537f8b4ec12ff8297337b67
Hugues Bernet-Rollande <hugues@xdev.fr>
2016-01-29 00:13:06 +0100

    Stack testing: re-implemented class for easier in-line reading

--- Pages/Testing our Stack.xcplaygroundpage/Contents.swift
+++ Pages/Testing our Stack.xcplaygroundpage/Contents.swift
@@ -7,6 +7,27 @@ Even with some **weak tests**, implementation dependent...
 
 */
 
+public class Stack<T> {
+    public var items:[T]
+    
+    public init(){
+        self.items = []
+    }
+    
+    public func push(value:T) {
+        items.append(value)
+    }
+    
+    public func pop() -> T? {
+        return items.popLast()
+    }
+    
+    public func peek() -> T? {
+        if items.count == 0 { return nil }
+        return items[items.count - 1]
+    }
+}
+
 import XCTest
 
 class IntStackTests : XCTestCase {
