b96d941addb45c91996392ebb1cbfff161558a32
Hugues Bernet-Rollande <hugues@xdev.fr>
2016-01-28 18:16:40 +0100

    MaxStack: Adding tests

--- Pages/MaxStack.xcplaygroundpage/Contents.swift
+++ Pages/MaxStack.xcplaygroundpage/Contents.swift
@@ -16,6 +16,41 @@ Also, the homogeneous requirement of Generics allows us to avoid comparing Apple
 
 */
 
+//: Let's test it (a tiny bit)
+
+import XCTest
+
+class MaxStackTests : XCTestCase {
+    
+    func testEmpty() {
+        let s = MaxStack<Int>()
+        XCTAssertEqual(s.getMax(), nil)
+    }
+    
+    func testMax() {
+        let s = MaxStack<Int>()
+        s.push(3)
+        s.push(1)
+        XCTAssertEqual(s.getMax(), 3)
+    }
+    
+    func testLastMax() {
+        let s = MaxStack<Int>()
+        s.push(2)
+        s.push(1)
+        s.push(3)
+        XCTAssertEqual(s.getMax(), 3)
+        s.pop()
+        XCTAssertEqual(s.getMax(), 2)
+    }
+    
+    /*:
+    TODO: Other tests related to stack behavior
+    
+    Question: How could we adapt our Stack and Unit Test to be able to re-use it here?
+    */
+}
+
 class MaxStack<T:Comparable> {
     var items:Stack<T>
     var maxStack:Stack<T>
@@ -53,6 +88,82 @@ class MaxStack<T:Comparable> {
     }
 }
 
-//: We should have some tests for this...
-
 //: [Next](@next)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class PlaygroundTestObserver : NSObject, XCTestObservation {
+    @objc func testCase(testCase: XCTestCase, didFailWithDescription description: String, inFile filePath: String?, atLine lineNumber: UInt) {
+        print("Test failed on line \(lineNumber): \(testCase.name), \(description)")
+    }
+    
+    @objc func testCaseWillStart(testCase: XCTestCase) {
+        print("Running: \(testCase.name)")
+    }
+}
+
+let observer = PlaygroundTestObserver()
+let center = XCTestObservationCenter.sharedTestObservationCenter()
+center.addTestObserver(observer)
+
+struct TestRunner {
+    
+    func runTests(testClass:AnyClass) {
+        print("Running test suite \(testClass)")
+        
+        let tests = testClass as! XCTestCase.Type
+        
+        // grab the default test suit of the test case
+        let testSuite = tests.defaultTestSuite()
+        
+        // run the test
+        testSuite.runTest()
+        
+        // collect and display informations about the tests run
+        let testRun = testSuite.testRun as! XCTestSuiteRun
+        
+        print("Ran \(testRun.executionCount) tests in \(testRun.testDuration)s with \(testRun.totalFailureCount) failures")
+    }
+    
+}
+
+TestRunner().runTests(MaxStackTests)
+
+
+
